df8e3ac32ef054430fbdee88f50520e6de227c8f
zhangjiongxuan <zhangjiongxuan@360.cn>
2017-07-07 22:03:42 +0800

    Re-design SampleApplication class (extends RePluginApplication)

--- replugin-sample/host/app/src/main/java/com/qihoo360/replugin/sample/host/SampleApplication.java
+++ replugin-sample/host/app/src/main/java/com/qihoo360/replugin/sample/host/SampleApplication.java
@@ -17,9 +17,13 @@
 package com.qihoo360.replugin.sample.host;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
+import com.qihoo360.replugin.DefaultRePluginCallbacks;
 import com.qihoo360.replugin.RePlugin;
 import com.qihoo360.replugin.RePluginApplication;
+import com.qihoo360.replugin.RePluginCallbacks;
 import com.qihoo360.replugin.RePluginConfig;
 
 /**
@@ -27,15 +31,6 @@ import com.qihoo360.replugin.RePluginConfig;
  */
 public class SampleApplication extends RePluginApplication {
 
-    @Override
-    protected RePluginConfig createConfig() {
-        RePluginConfig c = new RePluginConfig();
-
-        // FIXME RePlugin默认会对安装的外置插件进行签名校验，这里先关掉，避免调试时出现签名错误
-        c.setVerifySign(!BuildConfig.DEBUG);
-
-        return c;
-    }
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -43,5 +38,55 @@ public class SampleApplication extends RePluginApplication {
 
         // FIXME 允许接收rpRunPlugin等Gradle Task，发布时请务必关掉，以免出现问题
         RePlugin.enableDebugger(base, BuildConfig.DEBUG);
+    }
+
+    // ----------
+    // 自定义行为
+    // ----------
+
+    @Override
+    protected RePluginCallbacks createCallbacks() {
+        return new HostCallbacks(this);
+    }
+
+    /**
+     * RePlugin允许提供各种“自定义”的行为，让您“无需修改源代码”，即可实现相应的功能
+     */
+    @Override
+    protected RePluginConfig createConfig() {
+        RePluginConfig c = new RePluginConfig();
+
+        // 允许“插件使用宿主类”。默认为“关闭”
+        c.setUseHostClassIfNotFound(true);
+
+        // FIXME RePlugin默认会对安装的外置插件进行签名校验，这里先关掉，避免调试时出现签名错误
+        c.setVerifySign(!BuildConfig.DEBUG);
+
+        // FIXME 若宿主为Release，则此处应加上您认为"合法"的插件的签名，例如，可以写上"宿主"自己的。
+        // RePlugin.addCertSignature("AAAAAAAAA");
+
+        return c;
+    }
+
+    /**
+     * 宿主针对RePlugin的自定义行为
+     */
+    private class HostCallbacks extends DefaultRePluginCallbacks {
+
+        private static final String TAG = "HostCallbacks";
+
+        private HostCallbacks(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onPluginNotExistsForActivity(Context context, String plugin, Intent intent, int process) {
+            // FIXME 当插件"没有安装"时触发此逻辑，可打开您的"下载对话框"并开始下载。
+            // FIXME 其中"intent"需传递到"对话框"内，这样可在下载完成后，打开这个插件的Activity
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "onPluginNotExistsForActivity: Start download... p=" + plugin + "; i=" + intent);
+            }
+            return super.onPluginNotExistsForActivity(context, plugin, intent, process);
+        }
     }
 }
