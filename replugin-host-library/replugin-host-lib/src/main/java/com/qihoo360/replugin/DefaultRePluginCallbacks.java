c97f92e5b96ba1c15e499e4ccf60d0cfb27c7079
zhangjiongxuan <zhangjiongxuan@360.cn>
2017-07-10 20:10:57 +0800

    Add DefaultRePluginCallbacks & DefaultRePluginEventCallbacks for compatible 2.1.3 and below (deprecated)

---
+++ replugin-host-library/replugin-host-lib/src/main/java/com/qihoo360/replugin/DefaultRePluginCallbacks.java
@@ -0,0 +1,20 @@
+package com.qihoo360.replugin;
+
+import android.content.Context;
+
+/**
+ * @deprecated Use RePluginCallbacks instead
+ * @see RePluginCallbacks
+ * @author RePlugin Team
+ */
+
+public class DefaultRePluginCallbacks extends RePluginCallbacks {
+
+    /**
+     * @deprecated Use RePluginCallbacks instead
+     * @see RePluginCallbacks#RePluginCallbacks(Context)
+     */
+    public DefaultRePluginCallbacks(Context context) {
+        super(context);
+    }
+}
---
+++ replugin-host-library/replugin-host-lib/src/main/java/com/qihoo360/replugin/DefaultRePluginEventCallbacks.java
@@ -0,0 +1,20 @@
+package com.qihoo360.replugin;
+
+import android.content.Context;
+
+/**
+ * @deprecated Use RePluginEventCallbacks instead
+ * @see RePluginEventCallbacks
+ * @author RePlugin Team
+ */
+
+public class DefaultRePluginEventCallbacks extends RePluginEventCallbacks {
+
+    /**
+     * @deprecated Use RePluginEventCallbacks instead
+     * @see RePluginEventCallbacks#RePluginEventCallbacks(Context)
+     */
+    public DefaultRePluginEventCallbacks(Context context) {
+        super(context);
+    }
+}
