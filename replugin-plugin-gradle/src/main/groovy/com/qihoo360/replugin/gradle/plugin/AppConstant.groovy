8fac381ea0869606d9d88e8b294d256afd9465ed
wangyupeng1-iri <871791801@qq.com>
2017-07-12 18:29:15 +0800

    Add some gradle tasks

--- replugin-plugin-gradle/src/main/groovy/com/qihoo360/replugin/gradle/plugin/AppConstant.groovy
+++ replugin-plugin-gradle/src/main/groovy/com/qihoo360/replugin/gradle/plugin/AppConstant.groovy
@@ -34,14 +34,40 @@ class AppConstant {
     /** Task前缀 */
     def static final TASKS_PREFIX = "rp"
 
+
+    /** 用户Task:强制停止宿主app */
+    def static final TASK_FORCE_STOP_HOST_APP = TASKS_PREFIX + "ForceStopHostApp"
+
+    /** 用户Task:启动宿主app */
+    def static final TASK_START_HOST_APP = TASKS_PREFIX + "StartHostApp"
+
+    /** 用户Task:重启宿主app */
+    def static final TASK_RESTART_HOST_APP = TASKS_PREFIX + "RestartHostApp"
+
+
     /** 用户Task:安装插件 */
     def static final TASK_INSTALL_PLUGIN = TASKS_PREFIX + "InstallPlugin"
+
+    /** 用户Task:安装插件 */
+    def static final TASK_UNINSTALL_PLUGIN = TASKS_PREFIX + "UninstallPlugin"
 
     /** 用户Task:运行插件 */
     def static final TASK_RUN_PLUGIN = TASKS_PREFIX + "RunPlugin"
 
     /** 用户Task:安装并运行插件 */
     def static final TASK_INSTALL_AND_RUN_PLUGIN = TASKS_PREFIX + "InstallAndRunPlugin"
+
+
+    /** 配置例子 */
+    static final String CONFIG_EXAMPLE = '''
+// 这个plugin需要放在android配置之后，因为需要读取android中的配置项
+apply plugin: 'replugin-plugin-gradle\'
+repluginPluginConfig {
+    pluginName = "demo3"
+    hostApplicationId = "com.qihoo360.replugin.sample.host"
+    hostAppLauncherActivity = "com.qihoo360.replugin.sample.host.MainActivity"
+}
+'''
 
     private AppConstant() {}
 }
