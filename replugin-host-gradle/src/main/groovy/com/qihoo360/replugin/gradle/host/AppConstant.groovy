ece755cf8171a6ac8cfe87f0977aab335e6bf8a8
wangyupeng1-iri <871791801@qq.com>
2017-07-13 15:14:48 +0800

    Host gradle automatically synchronizes the version number

--- replugin-host-gradle/src/main/groovy/com/qihoo360/replugin/gradle/host/AppConstant.groovy
+++ replugin-host-gradle/src/main/groovy/com/qihoo360/replugin/gradle/host/AppConstant.groovy
@@ -22,8 +22,11 @@ package com.qihoo360.replugin.gradle.host
  */
 class AppConstant {
 
+    /** 版本号 */
+    def static final VER = "2.1.4"
+
     /** 打印信息时候的前缀 */
-    def static final TAG = "< replugin-host-v2.1.2 >"
+    def static final TAG = "< replugin-host-v${VER} >"
 
     /** 外部用户配置信息 */
     def static final USER_CONFIG = "repluginHostConfig"
