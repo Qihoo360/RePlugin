0904437d4a515f43e81119d24292a85804742d0d
cundong <cundong.liu@gmail.com>
2018-02-23 18:58:54 +0800

    the version was upgraded to v2.2.4

--- replugin-plugin-gradle/src/main/groovy/com/qihoo360/replugin/gradle/plugin/AppConstant.groovy
+++ replugin-plugin-gradle/src/main/groovy/com/qihoo360/replugin/gradle/plugin/AppConstant.groovy
@@ -23,7 +23,7 @@ package com.qihoo360.replugin.gradle.plugin
 class AppConstant {
 
     /** 版本号 */
-    def static final VER = "2.2.2"
+    def static final VER = "2.2.4"
 
     /** 打印信息时候的前缀 */
     def static final TAG = "< replugin-plugin-v${VER} >"
