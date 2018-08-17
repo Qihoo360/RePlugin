48eb8b9c3767805faab0d11d00968928cf6e67bd
forDream <gam2046@users.noreply.github.com>
2018-08-17 18:40:22 +0800

    Solve #533 #581

--- replugin-host-gradle/src/main/groovy/com/qihoo360/replugin/gradle/host/RePlugin.groovy
+++ replugin-host-gradle/src/main/groovy/com/qihoo360/replugin/gradle/host/RePlugin.groovy
@@ -111,6 +111,15 @@ public class Replugin implements Plugin<Project> {
                                     //3.0.0之前，直接使用
                                     manifestFile = file
                                 }
+
+                                if (!manifestFile.exists()) {
+                                    def abi = output.filters.find { it.filterType == OutputFile.ABI }?.identifier
+
+                                    if (abi != null) {
+                                        manifestFile = new File(file, "${abi}/AndroidManifest.xml")
+                                    }
+                                }
+
                                 //检测文件是否存在
                                 if (manifestFile != null && manifestFile.exists()) {
                                     println "${AppConstant.TAG} handle manifest: ${manifestFile}"
